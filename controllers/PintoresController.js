0ce588f245ac741cbe3d73a8e28e1a564e0145b2
Jesus <jpalaciosmendias0@gmail.com>
2020-06-03 03:06:45 -0600

    Jesus Emanuel Palacios Mendias 4-P
    actualización de los datos de la pagina para el examen

---
+++ controllers/PintoresController.js
@@ -0,0 +1,25 @@
+let mongoose = require('mongoose');
+
+let Pintores =  require('../models/Pintores');
+
+let pintoresController = {};
+
+
+pintoresController.list = (req, res)=>{
+    Pintores.find({})
+    .limit(20)
+    .skip(0)
+    .exec((err, pintor)=>{
+        if(err){
+            console.log('Error: ', err)
+        }
+        res.render('../views/index',{
+            pintores: pintor,
+            titulo: "Listado de pintores",
+            yaer: new Date().getDate()
+        })
+    })
+
+};
+
+module.exports = pintoresController;
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+
+const PUERTO = process.env.PORT || 3000;
+
+let pintoresRouter = require('./routes/pintor');
+
+//sitio web y el HBS
+const app = express();
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/piblic'));
+
+app.use('/', pintoresRouter);
+
+//conexion con mongoDB
+mongoose.Promise = global.Promise;
+const cadena = 'mongodb+srv://PalaciosJesus:MAGISTER99@palaciosmendias-23ghu.mongodb.net/Sitio?retryWrites=true&w=majority'
+mongoose.connect(cadena, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        console.log('Conexion establecida');
+    })
+    .catch(err => console.log(err));
+
+
+//arrancar el servidor
+app.listen(PUERTO, ()=>{
+    console.log('Escuchando el puerto...')
+});
+
